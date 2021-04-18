62163575f1d84aa5fe2ba9180e6863c140d4814b
John <johnra2@illinois.edu>
2021-04-18 17:22:50 -0500

    Added activation button code

--- adxl/adxl.ino
+++ adxl/adxl.ino
@@ -1,15 +1,32 @@
 #include "ADXL335.h"
 
 ADXL335 accelerometer;
+bool isOff = true;
+//String webServer = 
+int powerButtonPin = 1;
+bool powerButtonPressed = false;
+int speaker = 2;
+int led = 3;
+
 void setup()
 {
   Serial.begin(115200);
+
+  pinMode(powerButtonPin, INPUT);
+  pinMode(speaker, OUTPUT);
+  pinMode(led, OUTPUT);
+  
   accelerometer.begin();
 }
 void loop()
 {
+  Serial.println(digitalRead(powerButtonPin));
+  //if (isOff)
+  
   float ax,ay,az;
   accelerometer.getAcceleration(&ax,&ay,&az);
-  //Serial.println(az);
+//  Serial.print(xvoltage); Serial.print(" ");
+//  Serial.print(yvoltage); Serial.print(" ");
+//  Serial.println(zvoltage);
   delay(5);
 }
